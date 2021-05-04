11da4c2b5677a53283c7f2bc7fd369c596468e90
John Nguyen <nguyen8585@gmail.com>
2021-05-04 12:19:06 -0700

    latest

--- src/APIRouterConstants.js
+++ src/APIRouterConstants.js
@@ -1,3 +1,3 @@
   
-export const API_URL = 'http://localhost:8080'
+export const API_URL = 'http://study-app.eba-fpkj2zn3.us-east-2.elasticbeanstalk.com'
 export const JPA_API_URL = 'http://localhost:8080/jpa'
